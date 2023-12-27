c66a1aac0d0447bad22ee4f2fd2a1513f20b1633
Protoy Barai <1905068@ugrad.cse.buet.ac.bd>
2023-12-27 15:25:49 +0600

    Hero Section added

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,78 +2,84 @@ import React from "react";
 import logo from "../../assets/images/logo.svg";
 // import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Button } from "@/components/ui/button"
-
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
-    <header className="header flex items-center shadow-lg">
-      <div className="container px-[100px] py-[10px]">
+    <header className="header flex items-center shadow-lg z-10">
+      <div className="container px-[100px] py-[5px] ">
         <div className="flex items-center justify-between">
-          <div>
-            <img src={logo} className="w-[120px] object-cover" alt="" />
-          </div>
+          <div className="w-[650px] flex items-center justify-between">
+            <div>
+              <img src={logo} className="w-[120px] object-cover" alt="" />
+            </div>
 
-          <div className="navigation">
-            <ul className="menu flex items-center">
-              <li className="menu-item p-[10px]">
-                <NavLink
-                  to="/"
-                  className={(navClass) =>
-                    navClass.isActive ? "font-bold text-orange-500" : ""
-                  }
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="menu-item p-[10px]">
-                <NavLink
-                  to="/about"
-                  className={(navClass) =>
-                    navClass.isActive ? "font-bold text-orange-500" : ""
-                  }
-                >
-                  About
-                </NavLink>
-              </li>
-              <li className="menu-item p-[10px]">
-                <NavLink
-                  to="/doctors"
-                  className={(navClass) =>
-                    navClass.isActive ? "font-bold" : ""
-                  }
-                >
-                  Find a Doctor
-                </NavLink>
-              </li>
-              <li className="menu-item p-[10px]">
-                <NavLink
-                  to="/doctors"
-                  className={(navClass) =>
-                    navClass.isActive ? "font-bold" : ""
-                  }
-                >
-                  MediShop
-                </NavLink>
-              </li>
-              <li className="menu-item p-[10px]">
-                <NavLink
-                  to="/doctors"
-                  className={(navClass) =>
-                    navClass.isActive ? "font-bold" : ""
-                  }
-                >
-                  MediLab
-                </NavLink>
-              </li>
-            </ul>
+            <div className="navigation">
+              <ul className="menu flex items-center">
+                <li className="menu-item p-[10px] hover:underline hover:underline-offset-[15px]">
+                  <NavLink
+                    to="/"
+                    className={(navClass) =>
+                      navClass.isActive ? "font-bold text-orange-500 " : ""
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="menu-item p-[10px] hover:underline hover:underline-offset-[15px]">
+                  <NavLink
+                    to="/about"
+                    className={(navClass) =>
+                      navClass.isActive ? "font-bold text-orange-500" : ""
+                    }
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li className="menu-item p-[10px] hover:underline hover:underline-offset-[15px]">
+                  <NavLink
+                    to="/doctors"
+                    className={(navClass) =>
+                      navClass.isActive ? "font-bold" : ""
+                    }
+                  >
+                    Find a Doctor
+                  </NavLink>
+                </li>
+                <li className="menu-item p-[10px] hover:underline hover:underline-offset-[15px]">
+                  <NavLink
+                    to="/doctors"
+                    className={(navClass) =>
+                      navClass.isActive ? "font-bold" : ""
+                    }
+                  >
+                    MediShop
+                  </NavLink>
+                </li>
+                <li className="menu-item p-[10px] hover:underline hover:underline-offset-[15px]">
+                  <NavLink
+                    to="/doctors"
+                    className={(navClass) =>
+                      navClass.isActive ? "font-bold" : ""
+                    }
+                  >
+                    MediLab
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
 
           <div className="flex">
             {/* <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-[3px] px-4 rounded-full">
               Login/SignUp
             </button> */}
-            <Button className="bg-orange-500 hover:bg-orange-600 text-white py-[10px] font-bold rounded-full">Login/Sign Up</Button>
+            <Button
+              size="sm"
+              className="bg-orange-500 hover:bg-orange-600 text-white font-bold rounded-full"
+            >
+              Login/Sign Up
+            </Button>
           </div>
         </div>
       </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import heroBanner from "../assets/images/hero_banner.svg";
+import heroImage from "../assets/images/hero_image.svg";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div
+      className="h-[500px] mx-[150px] shadow-inner "
+      style={{
+        backgroundImage: `url(${heroBanner})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      {/* <img src={heroImage} alt=""  className="object-fill w-full h-full"/> */}
+      <div className="container flex items-center justify-between py-[50px] px-[100px]">
+        <div>
+          <h1 className="font-bold text-4xl w-[500px]">
+            Empowering Health, Enriching Lives, Your Wellness Partner{" "}
+          </h1>
+          <p className="text-xs">
+            We help patients reducing the hassles the face in our conventional
+            medical systems
+          </p>
+          <Button
+            size="sm"
+            className="bg-orange-500 hover:bg-orange-600 text-white font-bold my-[10px]"
+          >
+            Explore Us
+          </Button>
+        </div>
+        <div>
+          <img src={heroImage} alt="" className="object-cover w-[500px]" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
