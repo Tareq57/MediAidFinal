eb542c879eb404ec873c742c332dc6900e9df71e
Tareq57 <tareq57@gmail.com>
2024-01-29 12:34:46 +0600

    schema modified

--- backend/models/SpecializationSchema.js
+++
@@ -1,8 +0,0 @@
-import e from "express"
-import mongoose from "mongoose"
-
-const SpecializationSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-})
-
-export default mongoose.model("Specialization", SpecializationSchema)
